b6ee95e04e403f6666d0c0a620ceb4f261d981b0
Sai-Harshith-15 <saiharshith15.git@gmail.com>
2024-12-25 19:36:33 +0530

    created the new react and python project
    created the new react and python project

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Demo</h1>
+      <div>
+        <input type="text" placeholder="Enter Title" />
+        <input type="number" placeholder="Release Year" />
+        <button>Add Book</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
